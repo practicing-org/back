6021e1668a83a0562983ca947112e00733f2c3ee
joo01029 <62934181+joo01029@users.noreply.github.com>
2021-01-25 16:39:03 +0900

    rm cahched

--- server/api/search/search.ts
+++
@@ -1,9 +0,0 @@
-import {Router} from 'express';
-import auth from '../../middleware/auth';
-import searchBoard from './func/searchboard';
-import searchUser from './func/searchUser';
-const search = Router();
-
-search.get('/search/board/:board', auth.checkTokenForSignin, searchBoard);
-search.get('/search/user/:user', auth.checkTokenForSignin, searchUser);
-export default search;
--- server/index.ts
+++
@@ -1,6 +0,0 @@
-
-import app from './App';
-
-const port: number = Number(process.env.PORT) || 3030;
-
-app.listen(port);
