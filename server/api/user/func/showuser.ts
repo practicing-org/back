b782650a90c374c5c5a01808bd7b1e428bc1a214
joo01029 <62934181+joo01029@users.noreply.github.com>
2021-04-04 18:09:37 +0900

    back

--- server/api/user/func/showuser.ts
+++ server/api/user/func/showuser.ts
@@ -41,8 +41,8 @@ export default async(req:Request, res:Response, next:NextFunction)=>{
             for(let i = 0; i < findboard.length; i++){
 
                 findboard[i].user = {user_id:selectUser.user_Id,userName: selectUser.name, profile:profile.filename};
-                const boardImage = await db.image.findAll({raw:true, attributes:['filename'], where:{boardId:findboard[i].boardId}})
-			    findboard[i].images = boardImage;
+                const boardImage = await db.image.findAll({raw:true, attributes:[[Sequelize.fn('GROUP_CONCAT',Sequelize.col("filename")),'filename']], where:{boardId:findboard[i].boardId}})
+                findboard[i].images = boardImage.filename;
 
                 let likeNum = await db.like.findOne({raw:true, attributes:[[Sequelize.fn('COUNT', Sequelize.col('*')), 'number']], where:{boardId: findboard[i].boardId}})
                 findboard[i].likeNum = likeNum.number;
@@ -62,7 +62,7 @@ export default async(req:Request, res:Response, next:NextFunction)=>{
         
         
         //공계범위가 전체인 글과 친구의 글 내가쓴 글에서 이미 로드된 글을 제외한 20글들  
-        const query = "select board.* as images from board where board.boardId not in"+board_Ids+"and (`showId` = 'all' or (`showId` = 'friend' and (board.user_Id = ANY(select user_Id from friend where friend =:user_Id and user_Id = ANY(select friend from friend where user_Id =:user_Id and friend =:selectuser_Id)) )))GROUP BY board.boardId order by boardId desc limit 5"
+        const query = "select board.*, GROUP_CONCAT(file.filename) as images from board left outer join file using(boardId) where board.boardId not in"+board_Ids+"and (`showId` = 'all' or (`showId` = 'friend' and (board.user_Id = ANY(select user_Id from friend where friend =:user_Id and user_Id = ANY(select friend from friend where user_Id =:user_Id and friend =:selectuser_Id)) )))GROUP BY board.boardId order by boardId desc limit 5"
         let findboard:any;
         await db.sequelize.query(query, {replacements: {user_Id:user.user_Id, selectuser_Id:selectuser}}, { type: QueryTypes.SELECT }).then(
             function (result:any){
@@ -78,8 +78,6 @@ export default async(req:Request, res:Response, next:NextFunction)=>{
             let likeNum = await db.like.findOne({raw:true, attributes:[[Sequelize.fn('COUNT', Sequelize.col('*')), 'number']], where:{boardId: findboard[i].boardId}})
 			findboard[i].likeNum = likeNum.number;
 
-            const boardImage = await db.image.findAll({raw:true, attributes:['filename'], where:{boardId:findboard[i].boardId}})
-			 findboard[i].images = boardImage;
 
 			const like = await db.like.findOne({raw:true, where:{user_Id:user.user_Id, boardId:findboard[i].boardId}})
 			findboard[i].like = !!like;
