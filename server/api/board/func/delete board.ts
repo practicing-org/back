22a38b1d6dbc4f91d19c67c7279e920166eeb419
joo01029 <62934181+joo01029@users.noreply.github.com>
2021-05-04 17:34:00 +0900

    aa

--- server/api/board/func/delete board.ts
+++ server/api/board/func/delete board.ts
@@ -5,7 +5,9 @@ import db from '../../../model/dbcon';
 
 export default async (req:Request, res:Response, next:NextFunction)=>{
   const {boardId, userId} = req.body;
+  console.log(boardId, userId);
   if(boardId == null){
+    console.log("null")
     res.status(400).json({
       message:"null 불가능",
       result:0
@@ -17,7 +19,9 @@ export default async (req:Request, res:Response, next:NextFunction)=>{
     const board = await db.board.findOne({raw:true, where:{boardId:boardId, user_Id:user.user_Id}});
 
     if(board == null){
+      console.log("Not his board")
       res.status(400).json({
+        
         message:"이 유저의 게시글이 아님",
         result:0
       })
