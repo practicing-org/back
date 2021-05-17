37fb35d1ae6ab077498a547c3c6938ef7b32b4e8
joo01029 <62934181+joo01029@users.noreply.github.com>
2021-05-17 18:12:55 +0900

    heroku

--- server/api/user/func/changePassword.ts
+++ server/api/user/func/changePassword.ts
@@ -3,13 +3,14 @@ import db from '../../../model/dbcon';
 import crypto from 'crypto';
 
 export default async(req:Request, res:Response, next:NextFunction)=>{
-    let {hashPassword, changePassword} = req.body;
+    let {hashPassword, changePassword} = req.query;
     let userId = req.body.userId;
     try{
         const finduser = await db.user.findOne({raw:true, where:{userId:userId,password:hashPassword}});
         if(finduser == null){
           console.log('userId is null');
           res.status(400).json({
+              result:0,
               message:'존재하지 않는 유저'
           })
           return;
@@ -17,6 +18,7 @@ export default async(req:Request, res:Response, next:NextFunction)=>{
         if(!userId|| !changePassword){
           console.log('userId is null');
           res.status(401).json({
+              result:0,
               message:'you didn`t send your id'
           })
           return;
@@ -25,11 +27,13 @@ export default async(req:Request, res:Response, next:NextFunction)=>{
         
         await db.user.update({password:HashChangePassword},{where:{userId:userId}})
         res.json({
-            result:1
+            result:1,
+            message:"성공",
         })
     }catch(err){
         console.log(err);
         res.status(500).json({
+            result:0,
             message:"server has error now"
         })
     }
