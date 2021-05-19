abbf37520329187b0726f044bda6236b83c1815a
joo01029 <62934181+joo01029@users.noreply.github.com>
2021-05-19 11:37:08 +0900

    heroku

--- server/api/user/func/changeName.ts
+++ server/api/user/func/changeName.ts
@@ -2,33 +2,38 @@ import {Request, Response, NextFunction} from 'express';
 import db from '../../../model/dbcon';
 
 export default async(req:Request, res:Response, next:NextFunction)=>{
-    let {name} = req.body;
+    let {name} = req.query;
     let userId = req.body.userId;
     console.log(name);
     try{
-        const finduser = await db.user.findOne({raw:true, where:{userId:userId}});
-        if(finduser == null){
-            console.log('userId is null');
-            res.status(400).json({
-                message:'존재하지 않는 유저'
-            })
-            return;
-        }
         if(!userId|| !name){
             console.log('userId is null');
             res.status(400).json({
+                result:0,
                 message:'you send null'
             })
             return;
         }
+        const finduser = await db.user.findOne({raw:true, where:{userId:userId}});
+        if(finduser == null){
+            console.log('finUser is null');
+            res.status(400).json({
+                result:0,
+                message:'존재하지 않는 유저'
+            })
+            return;
+        }
+        
         await db.user.update({name:name},{where:{userId:userId}})
         res.json({
-            result:1
+            result:1,
+            message:"성공",
         })
     }catch(err){
         console.log(err);
         res.status(500).json({
-            message:"server has error now"
+            result:1,
+            message:"성공",
         })
     }
     
